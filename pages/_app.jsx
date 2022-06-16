553dda1f55368d5db1d4f1e598b1a84b4d4836aa
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-16 11:03:05 -0300

    refactor:  removendo importação do styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
