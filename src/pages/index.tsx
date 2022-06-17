5a8ca79f6ec6f50fe83de0352f3ec1ce515926be
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-16 22:45:57 -0300

    refactor: ajustando a home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Primeiro</h1>
-    </div>
+    <>
+      <Head>
+        <title>Board - Organizando suas tarefas</title>
+      </Head>
+      <div className={styles.container}>
+        <h1>Primeiro</h1>
+      </div>
+    </>
   );
 }
