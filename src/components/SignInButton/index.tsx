00078ba1c785066ffc5dc7ec4411a60d0770d73e
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-17 20:11:48 -0300

    feat: criando autenticação de usuário

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 export function SignInButton() {
-  const session = true;
+  const { data: session } = useSession();
+  console.log(session);
 
   return session ? (
-    <button type="button" className={styles.signInButton} onClick={() => {}}>
-      <img
-        src="https://sujeitoprogramador.com/steve.png"
-        alt="Foto do usuário"
-      />
-      Olá Wilian
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <img src={session.user.image} alt={session.user.name} />
+      Olá {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton} onClick={() => {}}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#FFB800" />
       Entrar com github
     </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import '../styles/global.scss';
 import { Header } from '../components/Header';
-
+import { SessionProvider } from 'next-auth/react';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />;
-    </>
+    </SessionProvider>
   );
 }
 
