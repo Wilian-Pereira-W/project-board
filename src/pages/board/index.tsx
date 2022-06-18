fb2ad5b41459d6fd32a1c328f4ea42e8b7028df1
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-18 13:45:28 -0300

    feat: adicionando a task no banco

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -4,11 +4,14 @@ import Head from 'next/head';
 import styles from './styles.module.scss';
 import { FiPlus, FiCalendar, FiEdit2, FiTrash } from 'react-icons/fi';
 import { getSession } from 'next-auth/react';
-import { BoardProps } from '../../interface/Board';
+import { BoardProps, TaskList } from '../../interface/Board';
 import firebase from '../../services/firebaseConnection';
+import { format } from 'date-fns';
+import Link from 'next/link';
 
-export default function Board({ user }: BoardProps) {
+export default function Board({ user, data }: BoardProps) {
   const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState<TaskList[]>(JSON.parse(data));
 
   const handleAddTask = async (e: FormEvent) => {
     e.preventDefault();
@@ -26,8 +29,17 @@ export default function Board({ user }: BoardProps) {
         userId: user.id,
         nome: user.nome,
       })
-      .then(() => {
-        console.log('sucesso');
+      .then((doc) => {
+        const data = {
+          id: doc.id,
+          created: new Date(),
+          createdFormated: format(new Date(), 'dd MMMM yyyy'),
+          tarefa: task,
+          userId: user.id,
+          nome: user.nome,
+        };
+        setTaskList([...taskList, data]);
+        setTask('');
       })
       .catch((error) => {
         console.log(error);
@@ -50,28 +62,35 @@ export default function Board({ user }: BoardProps) {
             <FiPlus size={25} color="#17181F" />
           </button>
         </form>
-        <h1>Você tem 2 tarefas!</h1>
+        <h1>
+          Você tem {taskList.length}{' '}
+          {taskList.length === 1 ? 'tarefa' : 'tarefas'}!
+        </h1>
         <section>
-          <article className={styles.taskList}>
-            <p>Aprender criar projetos usando Next JS</p>
-            <div className={styles.actions}>
-              <div>
+          {taskList.map((task) => (
+            <article key={task.id} className={styles.taskList}>
+              <Link href={`/board/${task.id}`}>
+                <p>{task.tarefa}</p>
+              </Link>
+              <div className={styles.actions}>
                 <div>
-                  <FiCalendar size={20} color="#FFB800" />
-                  <time>07 Junho 2022</time>
+                  <div>
+                    <FiCalendar size={20} color="#FFB800" />
+                    <time>{task.createdFormated}</time>
+                  </div>
+                  <button>
+                    <FiEdit2 size={20} color="#FFB800" />
+                    <span>Editar</span>
+                  </button>
                 </div>
+
                 <button>
-                  <FiEdit2 size={20} color="#FFB800" />
-                  <span>Editar</span>
+                  <FiTrash size={20} color="#FF3636" />
+                  <span>Excluir</span>
                 </button>
               </div>
-
-              <button>
-                <FiTrash size={20} color="#FF3636" />
-                <span>Excluir</span>
-              </button>
-            </div>
-          </article>
+            </article>
+          ))}
         </section>
       </main>
     </>
@@ -90,6 +109,23 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     };
   }
 
+  const tasks = await firebase
+    .firestore()
+    .collection('tarefas')
+    .where('userId', '==', session?.id)
+    .orderBy('created', 'asc')
+    .get();
+
+  const data = JSON.stringify(
+    tasks.docs.map((item) => {
+      return {
+        id: item.id,
+        createdFormated: format(item.data().created.toDate(), 'dd MMMM yyyy'),
+        ...item.data(),
+      };
+    }),
+  );
+
   const user = {
     nome: session?.user.name,
     id: session?.id,
@@ -97,6 +133,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   return {
     props: {
       user,
+      data,
     },
   };
 };
