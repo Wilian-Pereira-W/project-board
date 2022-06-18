51daedfe55554bfbce117953ea0ca9ec1f9a6857
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-18 13:44:47 -0300

    feat: implementado o type TaskList

--- src/interface/Board/index.ts
+++ src/interface/Board/index.ts
@@ -1,6 +1,16 @@
+export type TaskList = {
+  id: string;
+  created: string | Date;
+  createdFormated?: string;
+  tarefa: string;
+  userId: string;
+  nome: string;
+};
+
 export interface BoardProps {
   user: {
     id: string;
     nome: string;
   };
+  data: string;
 }
